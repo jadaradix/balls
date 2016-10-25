3511b601118e6f20d8cee6905b7fb12914369477
James Garner <j@jada.io>
2016-10-25 01:22:17 +0100

    Some of these parseInt calls are wrong - we do like zero values here.

--- src/js/types/ball.js
+++ src/js/types/ball.js
@@ -4,19 +4,19 @@
 // all arguments are optional
 const Ball = function Ball (x, y, colour, width, height, vx, vy) {
   // x: default width of the arena is 640px so centre via (640 / 2 - (32 / 2))
-  this.x = parseInt(x) || 304;
+  this.x = x || 304;
   // y: default height of the arena is 480px so centre via (480 / 2 - (32 / 2))
-  this.y = parseInt(y) || 224;
+  this.y = y || 224;
   // colour
   // this is a random hex code if undefined
   // see https://www.paulirish.com/2009/random-hex-color-code-snippets/
   this.colour = colour || "#" + Math.floor(Math.random() * 16777215).toString(16);;
-  // width/height/vx/vy: if foo is undefined or otherwise invalid, parseInt(foo) will return NaN which evaluates falsely
+  // width/height: if foo is undefined or otherwise invalid, parseInt(foo) will return NaN which evaluates falsely
   // this is better than just foo || 32 because 0 is falsey, meaning only non-0 values for foo would work correctly
   this.width = parseInt(width) || 32;
   this.height = parseInt(height) || 32;
-  this.vx = parseInt(vx) || (Math.random() * 5);
-  this.vy = parseInt(vy) || (Math.random() * -5);
+  this.vx = vx || (Math.random() * 5);
+  this.vy = vy || (Math.random() * -5);
   // allow method chaining
   return this;
 };
