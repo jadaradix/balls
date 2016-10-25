f457430712d33dd899e4a22ba8e0ab1f5a41f28e
James Garner <j@jada.io>
2016-10-25 01:26:36 +0100

    Some test coverage.

---
+++ test/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const Arena = require("../src/js/types/arena.js");
+const Ball = require("../src/js/types/ball.js");
+
+describe("An empty arena", () => {
+
+  const arena = new Arena();
+
+  it("has a colour", (done) => {
+    expect(arena).to.have.property("colour");
+    return done();
+  });
+
+  it("has no balls", (done) => {
+    expect(arena.balls).to.have.length(0);
+    return done();
+  });
+
+});
+
+
+describe("An arena with 3 calls to addBall", () => {
+
+  const arena = new Arena();
+  const ball = new Ball();
+  arena.addBall(ball);
+  arena.addBall(ball);
+  arena.addBall(ball);
+
+  it("has 3 balls", (done) => {
+    expect(arena.balls).to.have.length(3);
+    return done();
+  });
+
+});
+
+
+describe("A ball", () => {
+
+  const ball = new Ball();
+
+  it("has a colour", (done) => {
+    expect(ball).to.have.property("colour");
+    return done();
+  });
+
+  it("has x and y properties which are numbers", (done) => {
+    expect(ball.x).to.be.a("number");
+    expect(ball.y).to.be.a("number");
+    return done();
+  });
+
+});
